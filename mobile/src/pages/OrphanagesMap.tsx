3634997bbd9203347e81ad795717315938224628
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2020-10-16 14:03:01 -0300

    orphanage data, orphanage details and select map position pages

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,111 +1,136 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import api from '../services/api';
 
 import mapMarker from '../images/map-marker.png';
 
+interface Orphanage {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
+
 export default function OrphanagesMap() {
-    const navigation = useNavigation();
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+  const navigation = useNavigation();
 
-    function handleNavigateToOrphanageDetails() {
-        navigation.navigate('OrphanageDetails');
-    }
+  useEffect(() => {
+    api.get('orphanages').then(response => {
+      setOrphanages(response.data);
+    })
+  }, []);
 
-    return (
-        <View style={styles.container}>
-            <MapView
-                provider={PROVIDER_GOOGLE}
-                style={styles.map}
-                initialRegion={{
-                latitude: -21.6752572,
-                longitude: -49.7459211,
-                latitudeDelta: 0.008,
-                longitudeDelta: 0.008
-                }}
+  function handleNavigateToOrphanageDetails(id: number) {
+    navigation.navigate('OrphanageDetails', { id });
+  }
+
+  function handleNavigateToCreateOrphanage() {
+    navigation.navigate('SelectMapPosition');
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -21.6752572,
+          longitude: -49.7459211,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008
+        }}
+      >
+        {orphanages.map(orphanage => {
+          return (
+            <Marker
+              key={orphanage.id}
+              icon={mapMarker}
+              calloutAnchor={{
+                x: 2.8,
+                y: 0.85
+              }}
+              coordinate={{
+                latitude: orphanage.latitude,
+                longitude: orphanage.longitude
+              }}
             >
-                <Marker
-                icon={mapMarker}
-                calloutAnchor={{
-                    x: 2.8,
-                    y: 0.85
-                }}
-                coordinate={{
-                    latitude: -21.6752572,
-                    longitude: -49.7459211
-                }}
-                >
-                <Callout tooltip onPress={handleNavigateToOrphanageDetails}>
-                    <View style={styles.calloutContainer}>
-                    <Text style={styles.calloutText}>Casa da criança</Text>
-                    </View>
-                </Callout>
-                </Marker>
-            </MapView>
+              <Callout tooltip onPress={() => handleNavigateToOrphanageDetails(orphanage.id)}>
+                <View style={styles.calloutContainer}>
+                  <Text style={styles.calloutText}>{orphanage.name}</Text>
+                </View>
+              </Callout>
+            </Marker>
+          );
+        })}
+      </MapView>
 
-            <View style={styles.footer}>
-                <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
 
-                <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
-                <Feather name="plus" size={20} color="#FFF" />
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
+        <RectButton style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
+          <Feather name="plus" size={20} color="#FFF" />
+        </RectButton>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1
-    },
+  container: {
+    flex: 1
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center'
+  },
+
+  calloutText: {
+    color: '#0089A5',
+    fontFamily: 'Nunito_700Bold',
+    fontSize: 14
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    elevation: 3
+  },
   
-    map: {
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height
-    },
-  
-    calloutContainer: {
-      width: 160,
-      height: 46,
-      paddingHorizontal: 16,
-      backgroundColor: 'rgba(255, 255, 255, 0.8)',
-      borderRadius: 16,
-      justifyContent: 'center'
-    },
-  
-    calloutText: {
-      color: '#0089A5',
-      fontFamily: 'Nunito_700Bold',
-      fontSize: 14
-    },
-  
-    footer: {
-      position: 'absolute',
-      left: 24,
-      right: 24,
-      bottom: 32,
-      backgroundColor: '#FFF',
-      borderRadius: 20,
-      height: 56,
-      paddingLeft: 24,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      elevation: 3
-    },
-    
-    footerText: {
-      color: '#8FA7B3',
-      fontFamily: 'Nunito_700Bold'
-    },
-  
-    createOrphanageButton: {
-      width: 56,
-      height: 56,
-      backgroundColor: '#15C3D6',
-      borderRadius: 20,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }
+  footerText: {
+    color: '#8FA7B3',
+    fontFamily: 'Nunito_700Bold'
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15C3D6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
